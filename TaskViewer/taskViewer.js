56401faa6aac44ea52f7edf4ca21488dbbf741a8
yangyali <yangyali@gzac.org>
2017-10-13 11:23:03 +0800

    显示文件中的任务项列表

---
+++ TaskViewer/taskViewer.js
@@ -0,0 +1,45 @@
+
+
+    var tableBody = $("table#jobGrid>tbody");
+    
+    var taskEvaluation = new Vue({
+        el:'#taskEvaluation',
+        data:{
+
+        },
+        methods:{
+            loadFolder: loadFolder,
+        },
+    });
+
+
+    function loadFolder() {
+        loadTaskList("./Task/", tableBody);
+
+    }
+
+
+    //显示任务项列表
+    function loadTaskList(folder, tableBody){
+        $.get(folder, function(data) {
+            //console.log(data);
+            $(data).find("ul#files li a.icon-directory").each(function() {
+                var title = $(this).attr("title");
+                if (title == "..") {
+                    return;
+                }
+                //console.log(title);
+                $('table#jobGrid>tbody').append('<tr><td>'+title+'</td>/tr>');
+
+            });
+
+        });
+    }
+
+
+
+
+
+
+
+
