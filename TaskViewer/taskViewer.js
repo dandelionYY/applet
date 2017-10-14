f93355e3094b7f8aab44a5c7583165e1a9d2120c
yangyali <yangyali@gzac.org>
2017-10-14 14:51:12 +0800

    vue显示列表

--- TaskViewer/taskViewer.js
+++ TaskViewer/taskViewer.js
@@ -1,50 +1,57 @@
 
+var taskEvaluation = new Vue({
+    el:'#taskEvaluation',
+    data:{
+        taskList:[]
 
-    var taskEvaluation = new Vue({
-        el:'#taskEvaluation',
-        data:{
-            taskList:[]
-            
-        },
-        methods:{
-            loadFolder: function loadFolder() {
-                loadTaskList("./taskFile/");
+    },
+    methods:{
+        loadFolder: function loadFolder() {
+            loadTaskList("./taskFile/");
+        }
+    },
+});
+
+
+function loadTaskList(folder){
+    $.get(folder, function(data) {
+        var list = [];
+        $(data).find("ul#files li a.icon-directory").each(function() {
+            var title = $(this).attr('title');
+            if(title == ".."){
+                return;
             }
-        },
+
+            var titleList = {
+                title: title,
+            }
+            list.push(titleList);
+
+            taskEvaluation.taskList = list;
+            
+
+
+        })
+
     });
 
 
 
-    //1 显示任务项列表
-    function loadTaskList(folder){
-        $.get(folder, function(data) {
-            $(data).find("ul#files li a.icon-directory").each(function() {
-                var title = $(this).attr('title');
-                if(title == ".."){
-                    return;
-                }
-                var titleList = [];
-                titleList = title;
-
-                taskEvaluation.taskList = titleList;
-                console.log(taskEvaluation.taskList);
 
 
-            })
+
+}
 
 
 
 
 
 
-        });
 
 
 
 
 
-
-    }
 
 
 
