c2e0b83db0ad3f1c6b7a4fd09f74ce5a54e91c28
yangyali <yangyali@gzac.org>
2017-10-14 10:31:52 +0800

    vue循环得到文件夹内列表

--- TaskViewer/taskViewer.js
+++ TaskViewer/taskViewer.js
@@ -3,7 +3,7 @@
     var taskEvaluation = new Vue({
         el:'#taskEvaluation',
         data:{
-            /*taskLists:[]*/
+            taskLists:[]
             
         },
         methods:{
@@ -23,55 +23,18 @@
     var taskList = [];
     function loadTaskList(folder, tableBody){
         $.get(folder, function(data) {
-            //console.log(data);
-            /*taskEvaluation.taskLists = $(data).find("ul#files li a.icon-directory").attr("title");*/
 
-            $(data).find("ul#files li a.icon-directory").each(function(index) {
-                var title = $(this).attr("title");
-                if (title == "..") {
-                    return;
-                }
-                //tableBody.append('<tr><td>'+title+'</td>/tr>');
-
-                var taskFileUrl = folder + title + "/readme.md";
-                taskList.push(taskFileUrl);
-
-                $.get(taskFileUrl, function(data) {
-                    //console.log(data);
-                    var titleRow = $("<tr class='title' data-url='" + taskFileUrl + "'></tr>").appendTo(tableBody);
-                    titleRow.append("<td colspan='9'>" + title + " </td>");
-
-                    var start = data.indexOf("[BEGIN JOBS TABLE]");
-                    var stop = data.indexOf("[END JOBS TABLE]");
-                    var lines = data.slice(start, stop).split("\n");
-
-                    loadTaskJobs(titleRow,lines);
-                });
+            taskEvaluation.taskLists = $(data).find("ul#files li a.icon-directory");
+            //console.log(taskEvaluation.taskLists[1]);
+            
 
 
-            });
+
 
         });
-    }
 
-    //2 遍历文件夹下的所有任务项
-    function loadTaskJobs(titleRow,lines){
-        $.each(lines, function() {
-            var line = this.trim();
-            if(line.length == 0 || line.indexOf("编号") >=0 || line.indexOf("|---") >=0 ){
-                return true;
-            }
 
-            var tableBody = $("table#jobGrid>tbody");
-            var row = $("<tr class='data'></tr>").appendTo(tableBody);
-            $.each(line.split("|"), function(index) {
-                if(index == 9){
-                    return true;
-                }
-                $("<td>" + this + "</td>").appendTo(row);
 
-            });
-        });
 
 
 
